83f79b1cef9ce3b3d2f6e80efaaf8e7ea145ef03
Yevgeni Kushnarenko <sjenku@gmail.com>
2020-07-04 22:15:33 +0300

    Create:ListCollectionViewController

---
+++ Teacher's_Week/Controller/ListCollectionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ListCollectionViewController.swift
+//  Teacher's_Week
+//
+//  Created by jenia kushnarenko on 04/07/2020.
+//  Copyright © 2020 jenia kushnarenko. All rights reserved.
+//
+
+import UIKit
+
+
+class ListCollectionViewController:UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+        
+    }
+    
+}
