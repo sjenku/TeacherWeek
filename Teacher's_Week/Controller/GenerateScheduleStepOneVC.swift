12f536bf0ef90fae5987b222f3d35ea1e12a12cb
Yevgeni Kushnarenko <sjenku@gmail.com>
2020-07-06 18:09:41 +0300

    Create: GenerateStepOneInfoContainer

--- Teacher's_Week/Controller/GenerateScheduleStepOneVC.swift
+++ Teacher's_Week/Controller/GenerateScheduleStepOneVC.swift
@@ -20,10 +20,15 @@ class GenerateScheduleStepOneVC:UIViewController {
         return view
     }()
     
+    let infoContainer:GenerateStepOneInfoContainer = {
+        let view = GenerateStepOneInfoContainer()
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        view.backgroundColor = UIColor.MyTheme.darkBG
         setSubviews()
         setConstriants()
         
@@ -31,14 +36,19 @@ class GenerateScheduleStepOneVC:UIViewController {
     
     private func setSubviews() {
         view.addSubview(buttonsContainer)
+        view.addSubview(infoContainer)
     }
     
     private func setConstriants() {
         view.addConstraintsWithFormat(format: "H:|-16-[v0]-16-|", views: buttonsContainer)
+        view.addConstraintsWithFormat(format: "H:|-32-[v0]-32-|", views: infoContainer)
         
         let additionalConstraints = [
             buttonsContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            buttonsContainer.heightAnchor.constraint(equalToConstant: DeviceConfigurations.windowHeight * 0.55)
+            buttonsContainer.heightAnchor.constraint(equalToConstant: DeviceConfigurations.windowHeight * 0.55),
+            
+            infoContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            infoContainer.bottomAnchor.constraint(equalTo: buttonsContainer.topAnchor)
         ]
         
         additionalConstraints.forEach { (constraint) in
---
+++ Teacher's_Week/View/GenerateScheduleStepOneViews/GenerateStepOneInfoContainer.swift
@@ -0,0 +1,22 @@
+//
+//  GenerateStepOneInfoContainer.swift
+//  Teacher's_Week
+//
+//  Created by jenia kushnarenko on 06/07/2020.
+//  Copyright © 2020 jenia kushnarenko. All rights reserved.
+//
+
+import UIKit
+
+
+
+class GenerateStepOneInfoContainer : CustomView {
+    
+    override func setupView() {
+        super.setupView()
+        
+        backgroundColor = .cyan
+        
+    }
+    
+}
